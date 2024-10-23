d48e51a03eaf65562a8a279b4b3941dbc68a5a18
Md.Ashraful Islam <crackthecode10@gmail.com>
2024-10-24 02:32:51 +0600

    Task-4_Complete the Add todo section

--- TodoApp/Todo_App/src/components/Header.jsx
+++ TodoApp/Todo_App/src/components/Header.jsx
@@ -1,11 +1,35 @@
 import noteImage from '../assets/images/notes.png'
 import tickImage from '../assets/images/double-tick.png'
 import plusImage from '../assets/images/plus.png'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { added, allCompleted, clearCompleted } from '../redux/todos/action'
 const Header = () => {
+    const dispatch=useDispatch()
+
+    const [input,setInput]=useState('');
+    const handleInput=(e)=>{
+        setInput(e.target.value)
+    }
+
+    const submitHandler=(e)=>{
+        e.preventDefault();
+        dispatch(added(input));
+        setInput('');
+
+    }
+
+    const completeAllHandler=()=>{
+        dispatch(allCompleted())
+    }
+    const clearCompleteHandler=()=>{
+        dispatch(clearCompleted())
+    }
   return (
     <div>
                     <form
                         className="flex items-center bg-gray-100 px-4 py-4 rounded-md"
+                        onSubmit={submitHandler}
                     >
                         <img
                             src={noteImage}
@@ -16,6 +40,8 @@ const Header = () => {
                             type="text"
                             placeholder="Type your todo"
                             className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
+                            value={input}
+                            onChange={handleInput}
                         />
                         <button
                                 type="submit"
@@ -32,7 +58,7 @@ const Header = () => {
                     </form>
 
                     <ul className="flex justify-between my-4 text-xs text-gray-500">
-                        <li className="flex space-x-1 cursor-pointer">
+                        <li className="flex space-x-1 cursor-pointer" onClick={completeAllHandler}>
                             <img
                                 className="w-4 h-4"
                                 src={tickImage}
@@ -40,7 +66,7 @@ const Header = () => {
                             />
                             <span>Complete All Tasks</span>
                         </li>
-                        <li className="cursor-pointer">Clear completed</li>
+                        <li className="cursor-pointer" onClick={clearCompleteHandler}>Clear completed</li>
                     </ul>
                 </div>
   )
